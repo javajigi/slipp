1a7521e7e72a2310ec593ba700be7ab06b71501e
javajigi <javajigi@javajigi-desktop>
2012-07-29 12:34:56 +0900

    SocialUserRepository를 직접 사용하도록 수정함.

--- src/main/java/net/slipp/domain/user/SocialUserService.java
+++ src/main/java/net/slipp/domain/user/SocialUserService.java
@@ -11,7 +11,6 @@ import org.springframework.social.connect.ConnectionKey;
 import org.springframework.social.connect.ConnectionRepository;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.util.Assert;
-import org.springframework.util.MultiValueMap;
 
 public class SocialUserService {
 	@Resource(name = "usersConnectionRepository")
@@ -25,59 +24,27 @@ public class SocialUserService {
 		Assert.notNull(connection, "connection can't be null!");
 
 		ConnectionRepository connectionRepository = usersConnectionRepository.createConnectionRepository(userId);
-		if (!isUserIdAvailable(userId, connectionRepository)) {
+		if (!isUserIdAvailable(userId)) {
 			throw new ExistedUserException(userId + " already is existed user!");
 		}
 
 		connectionRepository.addConnection(connection);
 	}
 
-	private boolean isUserIdAvailable(String userId, ConnectionRepository connectionRepository) {
-		MultiValueMap<String, Connection<?>> connections = findAllConnections(connectionRepository);
-		if (connections == null) {
-			return true;
-		}
-
-		if (connections.isEmpty()) {
-			return true;
-		}
-
-		return false;
-	}
-
-	private MultiValueMap<String, Connection<?>> findAllConnections(ConnectionRepository connectionRepository) {
-		return connectionRepository.findAllConnections();
-	}
-
-	private MultiValueMap<String, Connection<?>> findAllConnections(String userId) {
-		ConnectionRepository connectionRepository = usersConnectionRepository.createConnectionRepository(userId);
-		return connectionRepository.findAllConnections();
-	}
-
-	private Connection<?> findFirstConnection(MultiValueMap<String, Connection<?>> connections) {
-		if (connections.isEmpty()) {
-			return null;
-		}
-
-		for (List<Connection<?>> connectionList : connections.values()) {
-			return connectionList.get(0);
-		}
-
-		return null;
-	}
-	
-	private ConnectionKey findConnectionKeyByUserId(String userId) {
-		MultiValueMap<String, Connection<?>> connections = findAllConnections(userId);
-		Connection<?> connection = findFirstConnection(connections);
-		ConnectionKey connectionKey = connection.getKey();
-		return connectionKey;
+	private boolean isUserIdAvailable(String userId) {
+		List<SocialUser> socialUsers = socialUserRepository.findsByUserId(userId);
+		return socialUsers.isEmpty();
 	}
 
 	public SocialUser findByUserId(String userId) {
 		Assert.notNull(userId, "userId can't be null!");
 		
-		ConnectionKey connectionKey = findConnectionKeyByUserId(userId);
-		return findByUserIdAndConnectionKey(userId, connectionKey);
+		List<SocialUser> socialUsers = socialUserRepository.findsByUserId(userId);
+		if (socialUsers.isEmpty()) {
+			return null;
+		}
+		
+		return socialUsers.get(0);
 	}
 
 	public SocialUser findByUserIdAndConnectionKey(String userId, ConnectionKey connectionKey) {
