223d968e1f3e585fd540a8c9d8c31e4c02c05f6a
javajigi <javajigi@gmail.com>
2012-09-17 18:28:18 +0900

    #19 네이버 oauth API 개발을 중담함. 추후 상황을 보고 다시 개발한다.

--- src/main/java/net/slipp/support/social/naver/Naver.java
+++
@@ -1,8 +0,0 @@
-package net.slipp.support.social.naver;
-
-import org.springframework.social.ApiBinding;
-import org.springframework.social.twitter.api.UserOperations;
-
-public interface Naver extends ApiBinding {
-	UserOperations userOperations();
-}
