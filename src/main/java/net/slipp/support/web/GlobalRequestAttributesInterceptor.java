eee22b73991ec0a50b53af63137079288ae3d037
javajigi <javajigi@javajigi-desktop>
2012-11-11 14:58:45 +0900

    #35 node 모듈인 stylus를 사용해 css 작업환경을 조성한다. node_modules 디렉토리가 필요없어 삭제함. 로컬 개발 환경에서만 stylus를 실행하도록 구현함. application-properties.xml의 environment 속성이 DEVELOPMENT일 때만 실행함.

--- src/main/java/net/slipp/support/web/GlobalRequestAttributesInterceptor.java
+++ src/main/java/net/slipp/support/web/GlobalRequestAttributesInterceptor.java
@@ -3,23 +3,46 @@ package net.slipp.support.web;
 import java.io.File;
 import java.io.IOException;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.slipp.support.security.SessionService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.Assert;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class GlobalRequestAttributesInterceptor extends HandlerInterceptorAdapter {
+	private static final Logger log = LoggerFactory.getLogger(GlobalRequestAttributesInterceptor.class);
+	
 	@Resource (name = "sessionService")
 	private SessionService sessionService;
+	
+	@Value("#{applicationProperties['environment']}")
+	private String environment;
+	
+	@PostConstruct
+	public void afterPropertiesSet() {
+		log.info("current environment : {}", environment);
+		Assert.notNull(environment, "Environment cann't be null!");
+	}
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		request.setAttribute("loginUser", sessionService.getLoginUser());
-		runStylus();
+		
+		if (isDevelopEnvironment()) {
+			runStylus();	
+		}
 		return super.preHandle(request, response, handler);
+	}
+
+	private boolean isDevelopEnvironment() {
+		return "DEVELOPMENT".equals(environment);
 	}
 	
 	private static void runStylus() throws IOException {
@@ -27,6 +50,6 @@ public class GlobalRequestAttributesInterceptor extends HandlerInterceptorAdapte
 		ProcessBuilder pb = new ProcessBuilder(file.getAbsolutePath() + "/styl.sh");
 		pb.directory(new File(file.getAbsolutePath()));
 		pb.start();
-		System.out.println("run stylus");
+		log.debug("run stylus");
 	}
 }
