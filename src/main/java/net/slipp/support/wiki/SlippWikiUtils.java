f677af7bb440cb804445ce6aa167ff48f116acbf
javajigi <javajigi@gmail.com>
2012-11-15 18:25:02 +0900

    이미지 파싱시 발생하는 버그 수정함.

--- src/main/java/net/slipp/support/wiki/SlippWikiUtils.java
+++ src/main/java/net/slipp/support/wiki/SlippWikiUtils.java
@@ -1,6 +1,7 @@
 package net.slipp.support.wiki;
 
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,18 +10,19 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 
 public class SlippWikiUtils {
-	private static Logger logger = LoggerFactory.getLogger(SlippWikiUtils.class);
+	private static Logger log = LoggerFactory.getLogger(SlippWikiUtils.class);
 
 	private static final Pattern IMAGE_WIKI_PATTERN = Pattern.compile("!([a-zA-Z0-9]{32})!");
 
 	public static List<String> createImageListFrom(String contents) {
-		logger.debug("content : {}", contents);
+		log.debug("content : {}", contents);
 		Matcher matcher = IMAGE_WIKI_PATTERN.matcher(contents);
 		List<String> images = Lists.newArrayList();
 		for (int i = 0; matcher.find(); i++) {
-			logger.debug("index : {}, result : {}", i, matcher.group(1));
+			log.debug("index : {}, result : {}", i, matcher.group(1));
 			if (!images.contains(matcher.group(1))) {
 				images.add(matcher.group(1));
 			}
@@ -38,9 +40,17 @@ public class SlippWikiUtils {
 
 	public static String replaceImages(String contents) {
 		Matcher matcher = IMAGE_WIKI_PATTERN.matcher(contents);
-		if (matcher.find()) {
-			contents = matcher.replaceAll(createImageHtml(matcher.group(1)));
+		
+		Set<String> matchString = Sets.newHashSet();
+		while (matcher.find()) {
+			log.debug("match : {}", matcher.group(1));
+			matchString.add(matcher.group(1));
 		}
+		
+		for (String each : matchString) {
+			contents = contents.replace("!" + each + "!", createImageHtml(each));
+		}
+		
 		return contents;
 	}
 	
