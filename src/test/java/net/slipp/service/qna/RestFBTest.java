ffa301043f5253d9a48d2eb1ec1ae7f864f6eb7f
javajigi <javajigi@gmail.com>
2013-05-01 18:49:18 +0900

    페이스북에서 post 조회 부분 구현 중

--- src/test/java/net/slipp/service/qna/RestFBTest.java
+++ src/test/java/net/slipp/service/qna/RestFBTest.java
@@ -1,42 +1,55 @@
 package net.slipp.service.qna;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import java.util.List;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
 import com.restfb.Parameter;
 import com.restfb.types.FacebookType;
 import com.restfb.types.Post;
 
-@Ignore
 public class RestFBTest {
-	private static Logger logger = LoggerFactory.getLogger(RestFBTest.class);
-	
-	private FacebookClient dut;
+    private static Logger logger = LoggerFactory.getLogger(RestFBTest.class);
 
-	@Before
-	public void setup() {
-		String accessToken = "access_token";
-		dut = new DefaultFacebookClient(accessToken);
-	}
+    private FacebookClient dut;
 
-	@Test
-	public void post() throws Exception {
-		String message = "글쓰기 테스트입니다.";
-		FacebookType response = dut.publish("me/feed", FacebookType.class, 
-		    Parameter.with("link", "http://www.slipp.net/questions/87"),
-		    Parameter.with("message", message));
-		String id = response.getId();
-		logger.debug("id : {}", id);
-		
-		Post post = dut.fetchObject(id, Post.class);
-		assertThat(post.getMessage(), is(message));
-	}
+    @Before
+    public void setup() {
+        String accessToken = "";
+        dut = new DefaultFacebookClient(accessToken);
+    }
+
+    @Test
+    public void post() throws Exception {
+        String message = "그룹에 글쓰기 테스트입니다.";
+        FacebookType response = dut.publish("me/feed", FacebookType.class,
+                Parameter.with("link", "http://www.slipp.net/questions/87"), Parameter.with("message", message));
+        String id = response.getId();
+        logger.debug("id : {}", id);
+    }
+
+    @Test
+    public void groupPost() throws Exception {
+        String message = "그룹에 글쓰기 테스트입니다.";
+        FacebookType response = dut.publish("387530111326987/feed", FacebookType.class,
+                Parameter.with("link", "http://www.slipp.net/questions/87"), Parameter.with("message", message));
+        String id = response.getId();
+        logger.debug("id : {}", id);
+    }
+
+    @Test
+    public void fetchPost() throws Exception {
+        Connection<Post> myFeed = dut.fetchConnection("me/posts", Post.class);
+        for (List<Post> myFeedConnectionPage : myFeed) {
+            for (Post post : myFeedConnectionPage) {
+                logger.debug("Post: " + post.getMessage());
+            }
+        }
+    }
 }
