c227d4cc21a52db5f520bcc8c10cc2091415909e
javajigi <javajigi@gmail.com>
2013-05-28 14:56:26 +0900

    로그인 과정에서 NullpointerException이 발생하면 자동 로그아웃 처리 되도록 수정함.

--- src/main/java/net/slipp/social/security/SlippSecurityAuthenticationFilter.java
+++ src/main/java/net/slipp/social/security/SlippSecurityAuthenticationFilter.java
@@ -10,10 +10,12 @@ import net.slipp.domain.user.SocialUser;
 import net.slipp.support.security.SessionService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.authentication.RememberMeServices;
@@ -49,6 +51,10 @@ public class SlippSecurityAuthenticationFilter extends AbstractAuthenticationPro
 		} else {
 			SocialUser signInDetails = (SocialUser) request.getSession().getAttribute(
 					SlippSecuritySignInAdapter.SIGN_IN_DETAILS_SESSION_ATTRIBUTE_NAME);
+			
+			if (signInDetails == null) {
+			    return new AnonymousAuthenticationToken("slippAnonymousAuthenticationToken", "anonymousUser", AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS"));
+			}
 			
 			SlippUser userDetails;
 			if (signInDetails.isSLiPPUser()) {
