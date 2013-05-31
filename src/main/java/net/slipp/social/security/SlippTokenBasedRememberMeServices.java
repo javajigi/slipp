8df8cba77a47a726b7ad185cac2f2983ad7e919b
javajigi <javajigi@gmail.com>
2013-05-31 17:03:21 +0900

    #125: 2주가 되지 않은 상황에서 일정 시간이 지나면 자동 로그아웃된다. 디버깅을 위한 info 로그 추가함.

--- src/main/java/net/slipp/social/security/SlippTokenBasedRememberMeServices.java
+++ src/main/java/net/slipp/social/security/SlippTokenBasedRememberMeServices.java
@@ -10,6 +10,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import net.slipp.domain.ProviderType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.RememberMeAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
@@ -23,6 +25,8 @@ import org.springframework.security.web.authentication.rememberme.InvalidCookieE
 import org.springframework.util.StringUtils;
 
 public class SlippTokenBasedRememberMeServices extends AbstractRememberMeServices {
+    private static Logger log = LoggerFactory.getLogger(SlippTokenBasedRememberMeServices.class);
+    
     private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
     /**
      * @deprecated Use with-args constructor
@@ -38,11 +42,14 @@ public class SlippTokenBasedRememberMeServices extends AbstractRememberMeService
     @Override
     protected UserDetails processAutoLoginCookie(String[] cookieTokens, HttpServletRequest request,
             HttpServletResponse response) {
-
+        log.info("processAutoLoginCookie start!");
+        
         if (cookieTokens.length != 4) {
             throw new InvalidCookieException("Cookie token did not contain 4" +
                     " tokens, but contained '" + Arrays.asList(cookieTokens) + "'");
         }
+        
+        log.info("cookieTokens userId : {}", cookieTokens[0]);
 
         long tokenExpiryTime;
 
@@ -59,8 +66,6 @@ public class SlippTokenBasedRememberMeServices extends AbstractRememberMeService
                     + new Date(tokenExpiryTime) + "'; current time is '" + new Date() + "')");
         }
 
-        // Check the user exists.
-        // Defer lookup until after expiry time checked, to possibly avoid expensive database call.
         ProviderType providerType = ProviderType.valueOf(cookieTokens[3]);
         SlippUser userDetails = getSlippUserDetails(providerType, cookieTokens[0]);
 
@@ -76,6 +81,8 @@ public class SlippTokenBasedRememberMeServices extends AbstractRememberMeService
                                                                                                     + "' but expected '" + expectedTokenSignature + "'");
         }
 
+        log.info("processAutoLogin success userId : {}", cookieTokens[0]);
+        
         return userDetails;
     }
     
@@ -119,6 +126,7 @@ public class SlippTokenBasedRememberMeServices extends AbstractRememberMeService
     @Override
     public void onLoginSuccess(HttpServletRequest request, HttpServletResponse response,
             Authentication successfulAuthentication) {
+        log.info("onLoginSuccess start!");
         String username = retrieveUserName(successfulAuthentication);
         String password = retrievePassword(successfulAuthentication);
         ProviderType providerType = retrieveProviderType(successfulAuthentication);
@@ -147,11 +155,13 @@ public class SlippTokenBasedRememberMeServices extends AbstractRememberMeService
         expiryTime += 1000L* (tokenLifetime < 0 ? TWO_WEEKS_S : tokenLifetime);
 
         String signatureValue = makeTokenSignature(expiryTime, username, password);
+        
+        log.info("cookie signatureValue : {}", signatureValue);
 
         setCookie(new String[] {username, Long.toString(expiryTime), signatureValue, providerType.name()}, tokenLifetime, request, response);
 
         if (logger.isDebugEnabled()) {
-            logger.debug("Added remember-me cookie for user '" + username + "', expiry: '"
+            logger.info("Added remember-me cookie for user '" + username + "', expiry: '"
                     + new Date(expiryTime) + "'");
         }
     }
