f3979056687c2c2ab0360491eb17a88d505ff1f6
parkjaesung <javajigi@gmail.com>
2015-03-28 08:30:31 +0900

    기존 버전과 같이 생성자를 통해 applicationUrl 전달하도록 설정 변경함.

--- src/main/java/net/slipp/social/security/MyProviderSignInController.java
+++ src/main/java/net/slipp/social/security/MyProviderSignInController.java
@@ -2,6 +2,8 @@ package net.slipp.social.security;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.social.connect.ConnectionFactoryLocator;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.web.ProviderSignInController;
@@ -12,10 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/signin")
 public class MyProviderSignInController extends ProviderSignInController {
+	private static final Logger log = LoggerFactory.getLogger(MyProviderSignInController.class);
+	
 	@Inject
 	public MyProviderSignInController(ConnectionFactoryLocator connectionFactoryLocator,
 			UsersConnectionRepository usersConnectionRepository, SignInAdapter signInAdapter) throws Exception {
 		super(connectionFactoryLocator, usersConnectionRepository, signInAdapter);
 		super.afterPropertiesSet();
+		log.debug("call parent");
 	}
 }
