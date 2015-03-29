78cfb38c2fb260c5a3595537e243631611558712
parkjaesung <javajigi@gmail.com>
2015-03-29 20:11:53 +0900

    spring 설정을 java config으로 변경한다. #203 메일 설정과 freemarker 설정까지 모두 변경함.

--- src/main/java/slipp/config/ApplicationConfig.java
+++ src/main/java/slipp/config/ApplicationConfig.java
@@ -3,14 +3,15 @@ package slipp.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@Import(value={PersistenceJPAConfig.class})
-@ImportResource(value={ "classpath:infrastructure.xml",
-						"classpath:applicationContext-social.xml", 
+@Import(value={PersistenceJPAConfig.class, InfraConfig.class})
+@ImportResource(value={ "classpath:applicationContext-social.xml", 
 						"classpath:applicationContext-cache.xml",
 						"classpath:applicationContext-profile.xml",
 						"classpath:applicationContext.xml"})
+@PropertySource("classpath:application-properties.xml")
 public class ApplicationConfig {
 
 }
--- src/main/java/slipp/config/PersistenceJPAConfig.java
+++ src/main/java/slipp/config/PersistenceJPAConfig.java
@@ -7,7 +7,6 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 import org.springframework.orm.jpa.JpaDialect;
@@ -19,7 +18,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-@PropertySource("classpath:application-properties.xml")
 public class PersistenceJPAConfig {
 	@Autowired
 	private Environment env;
