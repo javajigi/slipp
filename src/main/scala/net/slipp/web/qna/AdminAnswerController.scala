b53cc095a96b825cc1ac9186a46a6a6bca44a87f
javajigi <javajigi@gmail.com>
2015-06-01 19:31:49 +0900

    QnA Controller 중 일부 코드를 Scala 기반으로 변경함.

---
+++ src/main/scala/net/slipp/web/qna/AdminAnswerController.scala
@@ -0,0 +1,37 @@
+package net.slipp.web.qna
+
+import javax.annotation.Resource
+
+import net.slipp.domain.qna.Answer
+import net.slipp.domain.user.SocialUser
+import net.slipp.service.qna.QnaService
+import net.slipp.service.tag.TagService
+import net.slipp.support.web.argumentresolver.LoginUser
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+@Controller
+@RequestMapping(Array("/admin/questions/{questionId}/answers"))
+class AdminAnswerController(@Resource(name = "qnaService") qnaService: QnaService) {
+
+  @RequestMapping(Array("{answerId}/form"))
+  def updateForm(@LoginUser loginUser: SocialUser, @PathVariable questionId: Long, @PathVariable answerId: Long, searchTerm: String, model: Model) = {
+    model.addAttribute("question", qnaService.findByQuestionId(questionId))
+    model.addAttribute("answer", qnaService.findAnswerById(answerId))
+    model.addAttribute("searchTerm", searchTerm)
+    "admin/qna/answer"
+  }
+
+  @RequestMapping(value = Array("{answerId}"), method = Array(RequestMethod.PUT))
+  def update(@LoginUser loginUser: SocialUser, @PathVariable questionId: Long, @PathVariable answerId: Long, answer: Answer) = {
+    val originalAnswer = qnaService.findAnswerById(answerId)
+    qnaService.updateAnswer(originalAnswer.getWriter(), answer)
+    "redirect:/admin/questions/%d#answer-%d".format(questionId, answerId)
+  }
+
+  def this() = this(null)
+}
---
+++ src/main/scala/net/slipp/web/qna/AdminQuestionController.scala
@@ -0,0 +1,65 @@
+package net.slipp.web.qna
+
+import javax.annotation.Resource
+import javax.servlet.http.HttpSession
+import net.slipp.domain.qna.Question
+import net.slipp.domain.qna.QuestionDto
+import net.slipp.service.qna.QnaService
+import net.slipp.service.tag.TagService
+import net.slipp.web.UserForm
+import net.slipp.web.QnAPageableHelper._
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import com.typesafe.scalalogging.LazyLogging
+
+@Controller
+@RequestMapping(Array("/admin/questions"))
+class AdminQuestionController(
+  @Resource(name = "qnaService") qnaService: QnaService,
+  @Resource(name = "tagService") tagService: TagService) extends LazyLogging {
+  private val DefaultPageSize = 30
+
+  @RequestMapping(Array(""))
+  def index(page: Integer, searchTerm: String, model: Model) = {
+    logger.debug("search keyword : {}", searchTerm)
+    model.addAttribute("questions", qnaService.findsAllQuestion(searchTerm, createPageableByQuestionUpdatedDate(page, DefaultPageSize)))
+    model.addAttribute("searchTerm", searchTerm)
+    "admin/qna/list"
+  }
+
+  @RequestMapping(Array("/{id}"))
+  def show(@PathVariable id: Long, searchTerm: String, model: Model) = {
+    model.addAttribute("question", qnaService.showQuestion(id))
+    model.addAttribute("tags", tagService.findLatestTags())
+    model.addAttribute("user", new UserForm())
+    model.addAttribute("searchTerm", searchTerm)
+    "admin/qna/show"
+  }
+
+  @RequestMapping(value = Array("/{id}"), method = Array(RequestMethod.DELETE))
+  def delete(@PathVariable id: Long, searchTerm: String) = {
+    val question = qnaService.findByQuestionId(id)
+    qnaService.deleteQuestion(question.getWriter(), id)
+    "redirect:/admin/questions?searchTerm=%s".format(searchTerm)
+  }
+
+  @RequestMapping(Array("/{id}/form"))
+  def updateForm(@PathVariable id: Long, searchTerm: String, model: Model) = {
+    model.addAttribute("question", qnaService.findByQuestionId(id))
+    model.addAttribute("searchTerm", searchTerm)
+    "admin/qna/form"
+  }
+
+  @RequestMapping(value = Array("/{id}"), method = Array(RequestMethod.PUT))
+  def update(@PathVariable id: Long, searchTerm: String, updatedQuestion: QuestionDto) = {
+    logger.debug("Question : {}", updatedQuestion)
+    val originalQuestion = qnaService.findByQuestionId(id)
+    qnaService.updateQuestionByAdmin(originalQuestion.getWriter(), updatedQuestion)
+    "redirect:/admin/questions/%d?searchTerm=%s".format(id, searchTerm)
+  }
+}
