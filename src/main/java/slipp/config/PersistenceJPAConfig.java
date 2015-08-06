166319d96869dd12bf8d2ee144d23100172ba0ee
javajigi <javajigi@gmail.com>
2015-08-06 10:12:40 +0900

    #203 spring 설정을 java config으로 변경한다. jpa:repositories 태그를 EnableJpaRepositories로 변경함.

--- src/main/java/slipp/config/PersistenceJPAConfig.java
+++ src/main/java/slipp/config/PersistenceJPAConfig.java
@@ -3,12 +3,14 @@ package slipp.config;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
+import net.slipp.support.jpa.SlippRepositoryFactoryBean;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
@@ -18,6 +20,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
+@EnableJpaRepositories(basePackages="net.slipp.repository", repositoryFactoryBeanClass = SlippRepositoryFactoryBean.class)
 public class PersistenceJPAConfig {
 	@Autowired
 	private Environment env;
