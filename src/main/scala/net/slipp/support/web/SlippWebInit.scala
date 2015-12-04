929a5ff354547c5f53fbba592f530ba843cda73d
javajigi <javajigi@gmail.com>
2015-12-04 13:18:56 +0900

    web.xml을 제거하고 WebApplicationInitializer를 사용하도록 수정함.

---
+++ src/main/scala/net/slipp/support/web/SlippWebInit.scala
@@ -0,0 +1,44 @@
+package net.slipp.support.web
+
+import java.util.EnumSet
+import javax.servlet.{DispatcherType, ServletContext}
+
+import com.opensymphony.sitemesh.webapp.SiteMeshFilter
+import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter
+import org.springframework.web.WebApplicationInitializer
+import org.springframework.web.context.ContextLoaderListener
+import org.springframework.web.context.support.{XmlWebApplicationContext, AnnotationConfigWebApplicationContext}
+import org.springframework.web.filter.{CharacterEncodingFilter, DelegatingFilterProxy, HiddenHttpMethodFilter}
+import org.springframework.web.servlet.DispatcherServlet
+import slipp.config.ApplicationConfig
+
+class SlippWebInit extends WebApplicationInitializer {
+  override def onStartup(container: ServletContext): Unit = {
+    val appContext = new AnnotationConfigWebApplicationContext()
+    appContext.register(classOf[ApplicationConfig])
+    container.addListener(new ContextLoaderListener(appContext))
+
+    val cef = new CharacterEncodingFilter
+    cef.setEncoding("UTF-8")
+    cef.setForceEncoding(true)
+    container.addFilter("characterEncodingFilter", cef)
+      .addMappingForUrlPatterns(null, false, "/*")
+
+    container.addFilter("httpMethodFilter", classOf[HiddenHttpMethodFilter])
+      .addMappingForUrlPatterns(EnumSet.allOf(classOf[DispatcherType]), false, "/*")
+
+    container.addFilter("openEntityManagerInViewFilter", classOf[OpenEntityManagerInViewFilter])
+      .addMappingForUrlPatterns(EnumSet.allOf(classOf[DispatcherType]), false, "/*")
+
+    container.addFilter("springSecurityFilterChain", classOf[DelegatingFilterProxy])
+      .addMappingForUrlPatterns(EnumSet.allOf(classOf[DispatcherType]), false, "/*")
+
+    container.addFilter("sitemesh", classOf[SiteMeshFilter])
+      .addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST, DispatcherType.FORWARD), false, "/*")
+
+    val webApplicationContext = new XmlWebApplicationContext()
+    val dispatcher = container.addServlet("slipp", new DispatcherServlet(webApplicationContext))
+    dispatcher.setLoadOnStartup(1)
+    dispatcher.addMapping("/")
+  }
+}
