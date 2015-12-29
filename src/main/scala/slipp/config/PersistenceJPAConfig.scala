c3d3f61994ba6b6c462f93035b9dfc7b538b8321
javajigi <javajigi@gmail.com>
2015-12-29 15:46:52 +0900

    불필요한 파일 제거함.

--- src/main/scala/slipp/config/PersistenceJPAConfig.scala
+++ src/main/scala/slipp/config/PersistenceJPAConfig.scala
@@ -2,15 +2,13 @@ package slipp.config
 
 import javax.persistence.EntityManagerFactory
 
-import net.slipp.support.jpa.SlippRepositoryFactoryBean
 import org.apache.commons.dbcp2.BasicDataSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.context.annotation.{PropertySource, Bean, Configuration, Primary}
+import org.springframework.context.annotation.{Bean, Configuration, Primary, PropertySource}
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer
 import org.springframework.core.env.Environment
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.data.repository.Repository
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
 import org.springframework.orm.jpa.{JpaTransactionManager, LocalContainerEntityManagerFactoryBean}
 import org.springframework.transaction.annotation.EnableTransactionManagement
@@ -25,7 +23,7 @@ object PersistenceJPAConfig {
 @Configuration
 @PropertySource(Array("classpath:application-properties.xml"))
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages = Array("net.slipp.repository"), repositoryFactoryBeanClass = classOf[SlippRepositoryFactoryBean[_ <: Repository[_, _], _, _ <: Serializable]])
+@EnableJpaRepositories(basePackages = Array("net.slipp.repository"))
 class PersistenceJPAConfig {
   @Autowired var env: Environment = _
 
